a47d8a553825697cd0d208f2908fff1e386c10ff
fazlulkarimhridoy <fkhridoy4321@gmail.com>
2023-09-17 06:37:04 +0600

    calculated total credit & remaining credit

--- src/Components/Bookmarks/Bookmarks.jsx
+++ src/Components/Bookmarks/Bookmarks.jsx
@@ -1,16 +1,20 @@
 /* eslint-disable react/prop-types */
 
-const Bookmarks = ({ bookmarks }) => {
+const Bookmarks = ({ bookmarks, totalCredit, totalRemaining }) => {
     return (
-        <div className="bg-white p-6 rounded-2xl">
-            <h2 className="text-2xl font-bold text-[#2F80ED] pb-4">Credit Hour Remaining 7 hr</h2>
+        <div className="bg-white p-6 rounded-2xl h-[750px]">
+            <h2 className="text-2xl font-bold text-[#2F80ED] pb-4">Credit Hour Remaining {totalRemaining} hr</h2>
             <hr />
             <h3 className="text-3xl font-bold pt-4 pb-4">Course Name</h3>
-            {
-                bookmarks.map((item)=>(
-                    <li className="list-decimal" key={item.id}>{item.title}</li>
-                ))
-            }
+            <div className="pb-4">
+                {
+                    bookmarks.map((item) => (
+                        <li className="list-decimal" key={item.id}>{item.title}</li>
+                    ))
+                }
+            </div>
+            <hr />
+            <h3 className="text-base font-medium pt-4">Total Credit Hour : {totalCredit}hr</h3>
         </div>
     );
 };
--- src/Components/Courses/Courses.jsx
+++ src/Components/Courses/Courses.jsx
@@ -7,6 +7,8 @@ const Courses = () => {
     // all states
     const [course, setCourse] = useState([]);
     const [bookmarks, setBookmarks] = useState([]);
+    const [totalCredit, setTotalCredit] = useState(0);
+    const [totalRemaining, setTotalRemaining] = useState(20);
 
     // side effects
     useEffect(() => {
@@ -17,12 +19,28 @@ const Courses = () => {
 
     // handlers for events
     const handleBookmarks = (courseData) => {
-        const seclectedBookmarks = [...bookmarks, courseData];
-        setBookmarks(seclectedBookmarks);
+        const isExisted = bookmarks.find((item) => item.id === courseData.id);
+        let credit = courseData.hour;
+        if (isExisted) {
+            return alert("You have already selected this course.")
+        }
+        else {
+            bookmarks.forEach((item)=>{
+                credit += item.hour;
+            });
+            const remainingHour = 20-credit;
+            console.log(credit, remainingHour);
+            setTotalRemaining(remainingHour);
+            setTotalCredit(credit);
+            const seclectedBookmarks = [...bookmarks, courseData];
+            setBookmarks(seclectedBookmarks);
+
+        }
+
     };
 
     return (
-        <div className="flex justify-between gap-6 container mx-auto">
+        <div className="flex justify-around gap-6 container mx-auto">
             <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 w-[1000px] gap-6">
                 {
                     course.map((courseData) => (
@@ -32,7 +50,10 @@ const Courses = () => {
                     ))
                 }
             </div>
-            <Bookmarks bookmarks={bookmarks}></Bookmarks>
+            <Bookmarks 
+            bookmarks={bookmarks}
+            totalCredit={totalCredit}
+            totalRemaining={totalRemaining}></Bookmarks>
         </div>
     );
 };
